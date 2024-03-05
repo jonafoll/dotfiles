eaf2df789a6f9b1079352002b3cdab5e2ee11421
jonafoll <jonathan.folland@gmail.com>
2024-03-05 15:52:46 +0100

    added nvim

---
+++ nvim/lua/plugins/python.lua
@@ -0,0 +1,32 @@
+return {
+  opts = function(_, opts)
+    if require("lazyvim.util").has("nvim-dap-python") then
+      opts.dap_enabled = true
+    end
+    return vim.tbl_deep_extend("force", opts, {
+      name = {
+        "venv",
+        ".venv",
+        "env",
+        ".env",
+      },
+    })
+  end,
+
+  {
+    "nvim-neotest/neotest-python",
+  },
+
+  {
+    "mfussenegger/nvim-dap-python",
+  -- stylua: ignore
+  keys = {
+    { "<leader>dPt", function() require('dap-python').test_method() end, desc = "Debug Method", ft = "python" },
+    { "<leader>dPc", function() require('dap-python').test_class() end, desc = "Debug Class", ft = "python" },
+  },
+    config = function()
+      local path = require("mason-registry").get_package("debugpy"):get_install_path()
+      require("dap-python").setup(path .. "/venv/bin/python")
+    end,
+  },
+}
